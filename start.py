2d2b27b653c47497d9394629b6af8dcc54af86fc
REDskyTM <71713652+REDskyTM@users.noreply.github.com>
2023-01-07 01:26:08 +0600

    Update start.py

--- start.py
+++ start.py
@@ -1,11 +1,15 @@
 from pyrogram import Client, filters
 import random
 import json
+import os
+
+plugins = dict(root="plugins")
 
 app = Client(
 	"my_account",
 	api_id = "", #вставьте сюда api_id
 	api_hash = "" #сюда api_hash
+	plugins = plugins
 	)
 
 app.run()
